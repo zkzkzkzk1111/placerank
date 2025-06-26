167faf6f809c073085c554780d93babfc0d01507
kmj <zkzkzkzk1111@naver.com>
2025-06-26 16:45:45 +0900

    ㅁ

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,25 @@
-import AcmeLogo from '@/app/ui/acme-logo';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
-import Link from 'next/link';
+'use client'; 
 
-export default function Page() {
+import Header from '@/presentation/component/Header';
+import Left from '@/presentation/component/Left';
+import { RankMain } from '../presentation/component/RankMain';
+
+
+export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-        {/* <AcmeLogo /> */}
-      </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
-            <a href="https://nextjs.org/learn/" className="text-blue-500">
-              Next.js Learn Course
-            </a>
-            , brought to you by Vercel.
-          </p>
-          <Link
-            href="/login"
-            className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
-          >
-            <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
-          </Link>
-        </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+    <div className="flex flex-col h-screen">
+      {/* 상단 헤더 */}
+      <Header />
+      {/* 본문: 왼쪽 메뉴 + 오른쪽 콘텐츠 */}
+      <div className="flex flex-1">
+        <Left nickname="김이지" />
+
+        <div className="flex-1 p-6">
+          {/* 오른쪽 메인 콘텐츠 */}
+          <h1 className="text-xl font-bold mb-4">네이버 플레이스 상위노출</h1>
+          <RankMain />
         </div>
       </div>
-    </main>
+    </div>
   );
 }
